5cca51150d5bc5aa983e2b3c490ddccd698b00a1
seongeun park <skandianpark@gmail.com>
2023-01-06 20:46:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+const express = require('express')
+const socket = require('socket.io')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const io = socket(server)
+
+app.use('/css', express.static('./static/css'))
+app.use('/js', express.static('./static/js'))
+
+app.get('/', function(request, response) {
+    fs.readFile('./static/index.html', function(err, data){
+        if(err) {
+            response.send('error')
+        } else {
+          response.writeHead(200, {'Content-Type':'text/html'})
+          response.write(data)
+          response.end()
+        }
+    })
+})
+
+io.sockets.on('connection', function(socket){
+
+  socket.on('newUser', function(name) {
+    console.log(name + ' logged in')
+    socket.name = name 
+    io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + ' logged in'})
+  })
+
+  socket.on('message', function(data){
+    data.name = socket.name
+    console.log(data)
+    socket.broadcast.emit('update', data);
+  })
+  
+  socket.on('disconnect', function() {
+    console.log(socket.name + 'logged out')
+    socket.broadcast.emit('update', {type: 'disconnect', name:'SERVER', message: socket.name + '  logged out'});
+  })
+})
+
+server.listen(8080, function() {
+    console.log('server is activated..')
+})
+
